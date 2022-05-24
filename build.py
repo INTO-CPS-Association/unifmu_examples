d868ef5a03ff7b7d8db3b5d4d2113f22dcdb48aa
Christian Legaard <clegaard@outlook.com>
2022-05-24 10:53:07 +0200

    Add script for archiving examples as zip directories

--- build.py
+++ build.py
@@ -1,7 +1,29 @@
 from argparse import ArgumentParser
+import shutil
+import os
+from pathlib import Path
 
 if __name__ == "__main__":
-    pass
 
     parser = ArgumentParser()
     parser.add_argument("--export")
+
+    _, dirs, _ = next(os.walk("examples"))
+
+    os.makedirs("exported", exist_ok=True)
+
+    for d in dirs:
+        path_out_no_suffix = Path("exported", d)
+        path_in = Path("examples", d)
+
+        shutil.make_archive(
+            base_name=path_out_no_suffix,
+            format="zip",
+            root_dir=path_in,
+            base_dir=".",
+        )
+
+        path_zip_suffix = f"{path_out_no_suffix}.zip"
+        path_no_suffix, _ = os.path.splitext(path_out_no_suffix)
+        path_fmu_suffix = f"{path_no_suffix}.fmu"
+        shutil.move(path_zip_suffix, path_fmu_suffix)
